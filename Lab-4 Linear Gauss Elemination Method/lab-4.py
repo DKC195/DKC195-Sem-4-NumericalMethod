5551cbabec2c18a0f098845afe267b07bcc1bfe5
Dhiraj KC <62998336+DKC195@users.noreply.github.com>
2023-03-27 10:23:57 +0545

    Lab 4 Gauss Elimination

---
+++ Lab-4 Linear Gauss Elemination Method/lab-4.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+a = np.array([[3, -2, 5, 0],
+             [4, 5, 8, 1],
+             [1, 1, 2, 1],
+             [2, 7, 6, 5]], float)
+b = np.array([2, 4, 5, 7], float)
+n = len(b)
+x = np.zeros(n, float)
+
+# Elimination Stage
+for i in range(n-1):
+  for j in range(i+1, n):
+    if a[j][i] == 0:
+      continue
+    factor = a[i][i]/a[j][i]
+    for k in range(i, n):
+      a[j][k] = a[i][k] - a[j][k]*factor
+    b[j] = b[i]-b[j]*factor
+
+# Back Substitution
+x[n-1] = b[n-1] / a[n-1][n-1]
+for i in range(n-2, -1, -1):
+  sum = 0
+  for j in range(i+1, n):
+    sum += a[i][j]*x[j]
+  x[i] = (b[i] - sum)/a[i][i]
+
+print("the solution is ", x)
